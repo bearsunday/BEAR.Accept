efe4ead986ca9db56695b9976a31c1f427a05b37
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-04-14 09:27:57 +0900

    update cs

--- src/Accept.php
+++ src/Accept.php
@@ -1,11 +1,8 @@
 <?php
 
 declare(strict_types=1);
-/**
- * This file is part of the BEAR.Accept package.
- *
- * @license http://opensource.org/licenses/MIT MIT
- */
+
+
 namespace BEAR\Accept;
 
 use Aura\Accept\AcceptFactory;
--- src/AcceptInterceptor.php
+++ src/AcceptInterceptor.php
@@ -1,11 +1,7 @@
 <?php
 
 declare(strict_types=1);
-/**
- * This file is part of the BEAR.Accept package.
- *
- * @license http://opensource.org/licenses/MIT MIT
- */
+
 namespace BEAR\Accept;
 
 use BEAR\Accept\Annotation\Available;
